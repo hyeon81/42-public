bf3f23d7706070206c9aa59af9dfa219c2d54f72
Hyeonji Kim <hyeokim2@c3r6s4.42seoul.kr>
2022-07-23 15:59:14 +0900

    add main.c

--- main.c
+++ main.c
@@ -6,14 +6,13 @@
 
 #include "get_next_line.h"
 char *get_next_line(int fd);
-
 int main()
 {
 	int             fd;
-	int             i;
+	// int             i;
 	int             j;
 	char    		*line = 0;
-	char			*lineadress[66];
+	// char			*lineadress[66];
 
 	j = 1;
 	if (!(fd = open("check.txt", O_RDONLY)))
@@ -21,18 +20,19 @@ int main()
 		printf("\nError in open\n");
 		return (0);
 	}
-	while ((i = get_next_line(fd)) > 0)
+	while (get_next_line(fd) != 0)
 	{
+		line = get_next_line(fd);
 		printf("%s\n", line);
-		lineadress[j - 1] = line;
-		j++;
+		// lineadress[j - 1] = line;
+		// j++;
 	}
 	printf("%s\n", line);
 	free(line);
 	close(fd);
 
-	while (--j > 0)
-		free(lineadress[j - 1]);
+	// while (--j > 0)
+		// free(lineadress[j - 1]);
 
 	return (0);
 }
