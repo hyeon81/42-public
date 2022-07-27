a6572d137477bfab66f14a397065f878b7afeb1e
Hyeonji Kim <hyeokim2@c3r4s6.42seoul.kr>
2022-07-27 21:09:02 +0900

    0908 m finish

--- get_next_line.c
+++ get_next_line.c
@@ -1,85 +1,33 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        :::      ::::::::   */
-// /*   get_next_line.c                                    :+:      :+:    :+:   */
-// /*                                                    +:+ +:+         +:+     */
-// /*   By: hyeokim2 <hyeokim2@student.42.fr>          +#+  +:+       +#+        */
-// /*                                                +#+#+#+#+#+   +#+           */
-// /*   Created: 2022/07/23 12:58:38 by hyeokim2          #+#    #+#             */
-// /*   Updated: 2022/07/23 16:57:00 by hyeokim2         ###   ########.fr       */
-// /*                                                                            */
-// /* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeokim2 <hyeokim2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 18:56:30 by hyeokim2          #+#    #+#             */
+/*   Updated: 2022/07/27 21:08:32 by hyeokim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-int	ft_strlen(char *s)
+void	ft_free(char **s1, char **s2, int flag)
 {
-	int	i;
-
-	i = 0;
-	while (s[i] != 0)
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	int		i;
-	int		len;
-	char	*arr;
-
-	i = 0;
-	len = 0;
-	while (s1[len] != '\0')
-		len++;
-	arr = (char *)malloc(sizeof(char) * (len + 1));
-	if (!arr)
-		return (0);
-	while (s1[i] != '\0')
-	{
-		arr[i] = s1[i];
-		i++;
-	}
-	arr[i] = '\0';
-	return (arr);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*str;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	str = (char *)malloc(sizeof(char) * 
-	(ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!str)
-		return (0);
-	while (s1[i] != 0)
-	{
-		str[j] = s1[i];
-		j++;
-		i++;
-	}
-	i = 0;
-	while (s2[i] != 0)
-	{
-		str[j] = s2[i];
-		j++;
-		i++;
-	}
-	str[j] = '\0';
-	return (str);
+	free(*s1);
+	if (flag == 1)
+		*s1 = *s2;
+	else
+		*s1 = 0;
 }
 
 char	*ft_split_line(const char *s1, int start, int end)
 {
 	char	*arr;
-	int	i = 0;
+	int		i;
 
-	arr = (char *)malloc(sizeof(char) * (end - start + 2));
+	i = 0;
+	arr = (char *)malloc(sizeof(char) * (end - start + 1));
 	if (!arr)
 		return (0);
 	while ((s1[start] != 0) && (start <= end))
@@ -92,83 +40,68 @@ char	*ft_split_line(const char *s1, int start, int end)
 	return (arr);
 }
 
-int	ft_strchr(const char *s, int c)
+char	*ft_return_last(char **backup)
 {
-	int	i;
+	char	*temp;
+	char	*line;
+	int		newline_idx;
 
-	i = 0;
-	while (s[i] != 0)
-	{
-		if (s[i] == c)
-			return (i);
-		i++;
-	}
-	return (-1);
-}
-
-char *ft_return_last(char **backup)
-{
-	char *temp;
-	char *line;
-	int newline_idx;
-	
 	if (*backup[0] == '\0')
 	{
-		// printf("case1; %s\n", *backup);
-		free(*backup);
-		*backup = 0;
+		ft_free(backup, 0, 0);
 		return (0);
 	}
-	// buf[line_size] = '\0';
-	// printf("-------return last--------\n");
-	// printf("buf: %s\n", buf);
-	// printf("backup: %s\n", *backup);
-	// *backup = temp;
-	// printf("temp: %s\n", temp);
-
-	if ((newline_idx = ft_strchr(*backup, '\n')) >= 0)
+	if ((ft_strchr(*backup, '\n')) >= 0)
 	{
+		newline_idx = ft_strchr(*backup, '\n');
 		line = ft_split_line(*backup, 0, newline_idx);
 		temp = ft_split_line(*backup, newline_idx + 1, ft_strlen(*backup));
-		free(*backup);
-		*backup = temp;
+		ft_free(backup, &temp, 1);
 		return (line);
 	}
 	temp = ft_strdup(*backup);
-	free(*backup);
-	*backup = 0;
+	ft_free(backup, 0, 0);
 	return (temp);
 }
 
-char *get_next_line(int fd)
+char	*ft_make_line(char **backup, char *buf)
 {
-	char buf[BUFFER_SIZE + 1];
-	static char *backup;
-	char *line;
-	char *temp;
-	int line_size;
-	int newline_idx;
- 
-	if (((fd < 0) || (fd > 256)) || (BUFFER_SIZE <= 0))
+	char	*temp;
+	char	*line;
+	int		newline_idx;
+
+	temp = ft_strjoin(*backup, buf);
+	ft_free(backup, &temp, 1);
+	if (ft_strchr(*backup, '\n') >= 0)
+	{	
+		newline_idx = ft_strchr(*backup, '\n');
+		line = ft_split_line(*backup, 0, newline_idx);
+		temp = ft_split_line(*backup, newline_idx + 1, ft_strlen(*backup));
+		ft_free(backup, &temp, 1);
+		return (line);
+	}
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	char		buf[BUFFER_SIZE + 1];
+	static char	*backup;
+	int			line_idx;
+	char		*line;
+
+	if ((fd < 0) || (BUFFER_SIZE <= 0) || (fd > 256))
 		return (0);
 	if (!backup)
-		backup = ft_strdup(""); 
-	while ((line_size = read(fd, buf, BUFFER_SIZE)) > 0)
+		backup = ft_strdup("");
+	line_idx = read(fd, buf, BUFFER_SIZE);
+	while (line_idx > 0)
 	{
-		buf[line_size] = '\0';
-		temp = ft_strjoin(backup, buf);
-		free(backup);
-		backup = temp;
-		if ((newline_idx = ft_strchr(backup, '\n')) >= 0)
-		{
-			line = ft_split_line(backup, 0, newline_idx);
-			temp = ft_split_line(backup, newline_idx + 1, ft_strlen(backup));
-			free(backup);
-			backup = temp;
+		buf[line_idx] = '\0';
+		line = ft_make_line(&backup, buf);
+		if (!(line == 0))
 			return (line);
-		}
+		line_idx = read(fd, buf, BUFFER_SIZE);
 	}
-	line = ft_return_last(&backup);
-	return (line);
-
+	return (ft_return_last(&backup));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hyeokim2 <hyeokim2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 12:58:46 by hyeokim2          #+#    #+#             */
-/*   Updated: 2022/07/25 16:03:05 by hyeokim2         ###   ########.fr       */
+/*   Updated: 2022/07/27 20:05:13 by hyeokim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-char *get_next_line(int fd);
+
+char	*get_next_line(int fd);
+char	*ft_make_line(char **backup, char *buf);
+char	*ft_return_last(char **backup);
+char	*ft_split_line(const char *s1, int start, int end);
+void	ft_free(char **s1, char **s2, int flag);
+int		ft_strlen(char *s);
+char	*ft_strdup(char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strchr(const char *s, int c);
 
 #endif
