e85d9cb119a9246e196ccbf4cb465b8c3a491ef0
Hyeonji Kim <hyeokim2@c3r6s7.42seoul.kr>
2022-11-16 12:01:39 +0900

    finish

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hyeokim2 <hyeokim2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 12:06:23 by hyeokim2          #+#    #+#             */
-/*   Updated: 2022/11/16 11:52:51 by hyeokim2         ###   ########.fr       */
+/*   Updated: 2022/11/16 11:59:14 by hyeokim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,35 +31,30 @@ typedef struct s_stack
 	t_node	*tail;
 }	t_stack;
 
-//util1
 int		ft_atoi(char *str, int i, int sign);
 int		ft_isdigit(int c);
 void	ft_putstr(char *s);
 t_node	*ft_lstnew(int index);
 t_node	*ft_init_lstnew(int data, int index);
 
-//utils2
 char	**ft_split(char *s, char c);
 char	**ft_free_all(char **s, int idx);
 int		ft_double_strlen(char **str);
 void	error_exit(void);
 void	ft_exit(char *s);
 
-//init
 void	ft_init_stack(t_stack *a, t_stack *b);
 void	make_argv(int ac, char **av, t_stack *a, int i);
 void	make_index(t_stack *a);
 void	is_sorted(t_stack *a);
 void	is_overlapped(t_stack *a);
 
-//push_pop
 void	insert_node_top(int index, t_node *head);
 void	insert_node_bottom(int index, t_node *tail);
 void	remove_node_top(t_node *head);
 void	remove_node_bottom(t_node *tail);
 void	insert_new_node(int data, int index, t_node *tail);
 
-//sort
 void	a_to_b(t_stack *a, t_stack *b, int i, int chunk);
 void	make_b(t_stack *b, int max, int i);
 void	b_to_a(t_stack *a, t_stack *b);
@@ -68,7 +63,6 @@ void	sort_over_five(t_stack *a, t_stack *b);
 void	pb_and_change_value(t_stack *a, t_stack *b, int *i, int *size);
 int		check_descending(t_stack *a);
 
-//command list
 void	sa(t_stack *a, int flag);
 void	sb(t_stack *b, int flag);
 void	ss(t_stack *a, t_stack *b, int flag);
