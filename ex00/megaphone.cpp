d8ee9f22ccdea8091277ca89cb31c71700a267a9
Hyeonji Kim <guswly801@gmail.com>
2023-02-28 12:15:16 +0900

    little change

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,14 +5,15 @@
 int main(int ac, char **av)
 {
     std::string str;
+    
     if (ac == 1)
     {
-        //* LOUD AND UNBEARABLE FEEDBACK NOISE *
+        std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE" << std::endl;
         return (0);
     }
     int i = 0;
     int j = 0;
-    while (j < 5)
+    while (i < ac)
     {
        str += (toupper(av[1][i]));
        i++;
