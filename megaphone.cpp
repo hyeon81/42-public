c5a20109df994efb3df46b1f0569ba559095455d
Hyeonji Kim <guswly801@gmail.com>
2023-03-01 20:40:07 +0900

    finish ex00

--- megaphone.cpp
+++ megaphone.cpp
@@ -6,7 +6,7 @@ int main(int ac, char **av)
 {
     if (ac == 1)
     {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE" << std::endl;
         return (0);
     }
     int i = 1;
