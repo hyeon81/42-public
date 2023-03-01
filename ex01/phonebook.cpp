fd29e8aaa5459243d5e8c3387922f1c08609f4e4
Hyeonji Kim <guswly801@gmail.com>
2023-03-01 20:55:06 +0900

    init ex01

---
+++ ex01/phonebook.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str;
+
+    while (1)
+    {
+        std::cout << "PLEASE ENTER COMMAND: ";
+        std::cin >> str;
+
+        if (str == "EXIT")
+        {
+
+        }
+        else if (str == "ADD")
+        {
+
+        }
+        
+    }
+    
+
+}
