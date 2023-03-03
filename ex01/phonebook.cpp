e1ff8b5fdbc792abbd84f81f798688a521c31663
Hyeonji Kim <guswly801@gmail.com>
2023-03-03 20:47:10 +0900

    little change

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 #include <string>
 
+class Contact {
+    private:
+        std::string firstName;
+        std::string lastName;
+        std::string nickName;
+        std::string phoneNumber;
+        std::string darkestSecret;
+        int     idx;
+    
+    public:
+        Contact()
+        {
+            std::cout << "PLEASE INPUT FIRSTNAME: ";
+            std::cin >> firstName;
+            std::cout << "PLEASE INPUT LASTNAME: ";
+            std::cin >> lastName;
+            std::cout << "PLEASE INPUT NICKNAME: ";
+            std::cin >> nickName;
+            std::cout << "PLEASE INPUT PHONENUMBER: ";
+            std::cin >> phoneNumber;
+            std::cout << "PLEASE INPUT DARKEST SECRET: ";
+            std::cin >> darkestSecret;
+        }
+
+        ~Contact()
+        {
+        }
+};
+
+class PhoneBook {
+    Contact contacts[8];
+};
+
 int main()
 {
     std::string str;
@@ -11,15 +44,15 @@ int main()
         std::cin >> str;
 
         if (str == "EXIT")
-        {
-
-        }
+            break;
         else if (str == "ADD")
         {
-
+            
         }
-        
+        else if (str == "SEARCH")
+        {
+            
+        }
     }
-    
-
+    return (0);
 }
