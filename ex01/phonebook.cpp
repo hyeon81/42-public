e573347199ce6d8ce10b65037203b87d3b8efed2
Hyeonji Kim <guswly801@gmail.com>
2023-03-06 21:54:43 +0900

    add phonebook

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -23,29 +23,48 @@ class Contact {
             std::cout << "PLEASE INPUT DARKEST SECRET: ";
             std::cin >> this->darkestSecret;
         }
+
+        void showContact()
+        {
+            std::cout << "|     index| first name| last name|  nickname|" << std::endl;
+            std::cout << "|" << "index" << "|" << this->firstName << "|" << this->lastName << "|" << this->nickName << "|" << std::endl;
+        }
 };
 
 class PhoneBook {
     private:
         Contact contacts[8];
-        int     index;
+        int     number = 0;
 
     public:
-        void showContact()
+        void searchContact()
         {
-            std::cout << "|" << this->index << "|" << this->firstName << "|" << this->lastName << "|" << this->nickName << "|" << std::endl;
+            contacts[this->number % 8].showContact();
         }
-        
+
+        void addPhonebookContact()
+        {
+            contacts[this->number % 8].addContact();
+            this->number++;
+        }
+
         void showAllContacts()
         {
-            std::cout << "|     index| first name| last name|  nickname|" << std::endl;
-        }  
+            int i;
 
+            i = 0;
+            while (i < 8)
+            {
+                contacts[i].showContact();
+                i++;
+            }
+        }
 };
 
 int main()
 {
     std::string str;
+    PhoneBook phoneBook;
 
     while (1)
     {
@@ -56,11 +75,11 @@ int main()
             break;
         else if (str == "ADD")
         {
-            
+            phoneBook.addPhonebookContact();
         }
         else if (str == "SEARCH")
         {
-            
+            phoneBook.showAllContacts();            
         }
     }
     return (0);
