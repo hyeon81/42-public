613b6ffa8a57205b32dbbf810eb402ab0480a8de
Hyeonji Kim <guswly801@gmail.com>
2023-03-10 19:05:55 +0900

    semi finish

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -9,91 +9,139 @@ class Contact {
         std::string nickName;
         std::string phoneNumber;
         std::string darkestSecret;
-    
+        int         index;
+
     public:
-        void addContact()
+        void addContact(int index)
         {
-            std::cout << "PLEASE INPUT FIRSTNAME: ";
+            std::cout << "Please Input Firstname: ";
             std::cin >> this->firstName;
-            std::cout << "PLEASE INPUT LASTNAME: ";
+            std::cout << "Please Input Lastname: ";
             std::cin >> this->lastName;
-            std::cout << "PLEASE INPUT NICKNAME: ";
+            std::cout << "Please Input Nickname: ";
             std::cin >> this->nickName;
-            std::cout << "PLEASE INPUT PHONENUMBER: ";
+            std::cout << "Please Input Phone number: ";
             std::cin >> this->phoneNumber;
-            std::cout << "PLEASE INPUT DARKEST SECRET: ";
+            std::cout << "Please Input Darkest Secret: ";
             std::cin >> this->darkestSecret;
+            this->index = index;
         }
 
-        void showContact(int index)
+        void shortenContact(std::string str)
         {
-            std::cout << "|     index| first name| last name|  nickname|" << std::endl;
-            std::cout << "|" << index << std::setw(10) << "|"  << this->firstName << std::setw(10) << "|" << this->lastName << std::setw(10) << "|" << this->nickName << std::setw(10) << "|" << std::endl;
+            if (str.length() > 10)
+            {
+                std::cout << str.substr(0, 9);
+                std::cout << "." << "|" ;
+            }
+            else
+                std::cout << std::setw(10) << str << "|" ;
         }
+
+        void showContact()
+        {
+            std::cout << std::right;
+            std::cout << "|" ;
+            shortenContact(std::to_string(this->index));
+            shortenContact(this->firstName);
+            shortenContact(this->lastName);
+            shortenContact(this->nickName);
+            std::cout << std::endl;
+        }
+        
 };
 
 class PhoneBook {
     private:
         Contact contacts[8];
         int     number;
+        int     searchIdx;
 
     public:
         void searchContact()
         {
-            contacts[this->number % 8].showContact(this->number % 8);
+            while (1)
+            {
+                std::cout << "Please Select Index: ";
+                std::cin >> this->searchIdx;
+                if (searchIdx < 0 || searchIdx > 7 || number < searchIdx)
+                    std::cout << "Wrong Index... Please Select Valid Index" << std::endl;
+                else
+                {
+                    std::cout << std::right;
+                    std::cout << "|" ;
+                    contacts[0].shortenContact("index");
+                    contacts[0].shortenContact("first name");
+                    contacts[0].shortenContact("last name");
+                    contacts[0].shortenContact("nickname");
+                    std::cout << std::endl;
+                    contacts[this->searchIdx].showContact();
+                    break;
+                }
+            }
         }
 
         void addPhonebookContact()
         {
-            contacts[this->number % 8].addContact();
+            contacts[this->number % 8].addContact(this->number % 8);
             this->number++;
         }
 
         void showAllContacts()
         {
-            int i;
+            int i = 0;
 
-            i = 0;
+            std::cout << std::right;
+            std::cout << "|" ;
+            contacts[0].shortenContact("index");
+            contacts[0].shortenContact("first name");
+            contacts[0].shortenContact("last name");
+            contacts[0].shortenContact("nickname");
+            std::cout << std::endl;
+
             if (this->number > 7)
             {
-                // printf("here");
                 while (i < 8)
                 {
-                    contacts[i].showContact(i);
+                    contacts[i].showContact();
                     i++;
                 }
             }
             else
             {
-                // printf("here2");
                 while (i < this->number)
                 {
-                    contacts[i].showContact(i);
+                    contacts[i].showContact();
                     i++;
                 }
             }
+            searchContact();
         }
 };
 
-int main()
+int main(void)
 {
     std::string str;
     PhoneBook phoneBook;
 
     while (1)
     {
-        std::cout << "PLEASE ENTER COMMAND: ";
+        std::cout << "Please Input Command(ADD, SEARCH, EXIT): ";
         std::cin >> str;
 
-        if (str == "EXIT")
+        if (str == "EXIT" || str == "exit")
             break;
-        else if (str == "ADD")
+        else if (str == "ADD" || str == "add")
         {
             phoneBook.addPhonebookContact();
         }
-        else if (str == "SEARCH")
+        else if (str == "SEARCH" || str == "search")
         {
-            phoneBook.showAllContacts();            
+            phoneBook.showAllContacts();         
+        }
+        else
+        {
+            std::cout << "Command Not Found" << std::endl;
         }
     }
     return (0);
