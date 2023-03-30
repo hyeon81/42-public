bd109205a6f70e4cdbfbe80c8449841d61173508
hyeokim2 <guswly801@gmail.com>
2023-03-31 00:56:44 +0900

    finish

--- ex04/main2.cpp
+++ ex04/main2.cpp
@@ -2,30 +2,35 @@
 #include <iostream>
 #include <string>
 
-void replace_line(std::string &line, std::string s1, std::string s2)
+std::string replace_line(std::string line, std::string s1, std::string s2)
 {
     std::string::size_type first = 0;
-    std::string::size_type last;
+    std::string::size_type last = 0;
+    std::string content;
 
+    int len = s1.length();
     while (1)
     {
         if (s1.empty() || s2.empty())
         {
+            content += line.substr(0, line.length());
             std::cout << "s1 or s2 is empty. there is no change" << std::endl;
-            return ;
+            break;
         }
         last = line.find(s1, first); //여기서 first는 찾을 범위의 시작점. last는 찾은 경우의 값
         if (last == std::string::npos) //만약에 이제 다찾아서 끝을 만나면?
         {
             if (first == 0)
                 std::cout << "file doesn't include s1. there is no change" << std::endl;
-            return ;
+            content += line.substr(first, line.length());
+            break;
         }
-        line.erase(last, s1.length());
-        line.insert(first, s2);
+        content += line.substr(first, last - first);
+        content += s2;
         std::cout << "content 내용:: " << content << std::endl;
-        first = last + s2.length();
+        first = last + len;
     }
+    return (content);
 }
 
 int main(int ac, char **av)
@@ -48,7 +53,7 @@ int main(int ac, char **av)
         while (std::getline(in, line))
         {
             std::cout << "입력 받은 문자열 ::" << line << std::endl;
-            content << replace_line(line, s1, s2) << std::endl;
+            content += replace_line(line, s1, s2);
         }
     } else {
         std::cout << "file open is failed" << std::endl;
