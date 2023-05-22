00776f0933eda23927b0cc352cb59ca170621852
Hyeonji Kim <guswly801@gmail.com>
2023-05-22 21:47:51 +0900

    semi finish-2

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: hyeokim2 <hyeokim2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 21:33:10 by hyeokim2          #+#    #+#             */
-/*   Updated: 2023/05/22 21:33:43 by hyeokim2         ###   ########.fr       */
+/*   Updated: 2023/05/22 21:47:34 by hyeokim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,17 +71,17 @@ void	all_free(t_vars *vars)
 	free(vars->c.east);
 }
 
-void	leaks(void)
-{
-	system("leaks cub3D");
-}
+// void	leaks(void)
+// {
+// 	system("leaks cub3D");
+// }
 
 int	main(int argc, char	**argv)
 {
 	t_vars		vars;
 	t_player	p;
 
-	atexit(leaks);
+	// atexit(leaks);
 	if (argc != 2 || check_arg(argv[1]))
 		exit_with_err("Invalid filetype\n");
 	init_info(&vars, argv[1], &p);
