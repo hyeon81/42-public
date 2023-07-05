3e31d8d41801aafccdc66773e3d98d823bb3ea29
Hyeonji Kim <guswly801@gmail.com>
2023-07-05 20:31:12 +0900

    finish

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -3,23 +3,33 @@
 void ScalarConverter::convert(std::string &value)
 {
     double res;
-    res = strtod(value.c_str(), NULL);
+    char _char;
+    long long _int;
+    float _float;
+
+    char *stopstring;
+    res = strtod(value.c_str(), &stopstring);
+    
+    //if argv is char
+    std::string tmp(stopstring);
+    if (value.length() == 1 && res == 0 && tmp.length() == 1)
+        res = static_cast<double>(value[0]);
     
     //toChar
-    char _char;
     _char = static_cast<char>(res);
     if (value == "nan")
         std::cout << "char: impossible" << std::endl;
-    else if (isprint(_char))
+    else if (res > 31 && res < 127)
         std::cout << "char: " << _char << std::endl;
     else
-        std::cout << "char: Non displayable" << std::endl;//informative message?
+        std::cout << "char: Non displayable" << std::endl;
 
     //toInt
-    long long _int;
     _int = static_cast<long long>(res);
-    if (value == "nan" || _int < INT_MIN || _int > INT_MAX)
+    if (value == "nan")
         std::cout << "int: impossible" << std::endl;
+    else if (_int < INT_MIN || _int > INT_MAX)
+        std::cout << "int: overflow" << std::endl;
     else
     {
         _int = static_cast<int>(res);
@@ -27,22 +37,14 @@ void ScalarConverter::convert(std::string &value)
     }
 
     //toFloat
-    float _float;
     _float = static_cast<float>(res);
     std::cout << "float: " << _float;
     if (_float == static_cast<int>(res))
         std::cout << ".0";
     std::cout << 'f' << std::endl;
-        
 
-    //toDouble
-    // if (value == "nan")
-    //     std::cout << "double: nan" << std::endl;
     if (res == static_cast<int>(res))
         std::cout << "double: " << res << ".0" << std::endl;
     else
-        std::cout << "double: " << res << std::endl;
-    
-    //.0f 붙여줄때 문자열로 바꿔서 확인
-    //inf값 확인
+        std::cout << "double: " << res << std::endl;    
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,19 +5,19 @@ int main()
     Data ptr;
     uintptr_t u_ptr;
     Data *ptr2;
+    uintptr_t u_ptr2;
+    Data *ptr3;
     
     ptr.name = "new";
-    std::cout << "name: " << ptr.name << std::endl;
-    std::cout  << "ptr: " << &ptr << std::endl;
+    std::cout << "ptr_name: " << ptr.name << std::endl;
 
     u_ptr = Serializer::serialize(&ptr);
-    std::cout << "u_ptr: " << &u_ptr << std::endl;
-    std::cout << "u_ptr value: " << u_ptr << std::endl;
-
     ptr2 = Serializer::deserialize(u_ptr);
+    std::cout << "ptr2_name: " << ptr2->name << std::endl;
 
-    //값을 바꾸기
-    //확인
-    std::cout << "name: " << ptr2->name << std::endl;
-    std::cout << "ptr2: " << &ptr2 << std::endl;
+    ptr2->name = "change";
+    u_ptr2 = Serializer::serialize(ptr2);
+    ptr3 = Serializer::deserialize(u_ptr2);
+    std::cout << "ptr3_name: " << ptr3->name << std::endl;
+    
 }
