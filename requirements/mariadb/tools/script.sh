4032487c164b0efa8dbc5cfb82d638e54517b5ea
hyeon81 <guswly801@gmail.com>
2023-09-02 18:48:58 +0900

    update files

--- requirements/mariadb/tools/script.sh
+++ requirements/mariadb/tools/script.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+
+# MySQL 데이터베이스 초기화 및 폴더 준비
+rm -rf /var/lib/mysql/*
+mysql_install_db
+mkdir -p /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /run/mysqld
+
 service mariadb start
 
-mysql -u root "CREATE DATABASE ${DB_NAME};"
-mysql -u root "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -u root "GRANT ALL ON . TO '${DB_NAME}'@'localhost' WITH GRANT OPTION;"
-mysql -u root "FLUSH PRIVILEGES;"
+# MySQL에 SQL 명령어를 실행하는 부분을 mysql.sql 파일로 이동
+cat <<EOF > mysql.sql
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
 
+# SQL 파일을 MySQL에 적용
+mysql -u root < mysql.sql
+
+# MySQL 실행
+mysqld
+# mysql
