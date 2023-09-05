5c027d712231bca919d078d60684f6d44e8058d4
Hyeon Ji Kim <guswly801@gmail.com>
2023-09-05 17:49:25 +0900

    add

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+#!/bin/bash
+
 service mariadb start
 
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+mariadb -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+# mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${db1_pwd}';"
+mariadb -u root -e "FLUSH PRIVILEGES;"
+
+service mariadb stop
+mariadbd
 
 # MySQL 데이터베이스 초기화 및 폴더 준비
 # rm -rf /var/lib/mysql/*
