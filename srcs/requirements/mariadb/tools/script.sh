20d828cd2469d427d27c3927915e0818d134db3c
Hyeonji Kim <guswly801@gmail.com>
2023-09-09 13:15:32 +0900

    finish

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-echo "echo mariadb start"
+rm -rf /var/lib/mysql/*
+mysql_install_db
+mkdir -p /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /run/mysqld
+
 service mariadb start
 
 mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 mariadb -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 mariadb -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-# mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${db1_pwd}';"
 mariadb -u root -e "FLUSH PRIVILEGES;"
 
-echo "echo mariadb stop"
 service mariadb stop
-echo "echo mariadbd"
 
 mariadbd
--- srcs/requirements/wordpress/tool/script.sh
+++ srcs/requirements/wordpress/tool/script.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-# WordPress 다운로드
-echo "wp core download"
-# 파일 및 디렉토리 권한 설정
 chown -R www-data:www-data /var/www/html/
 chmod -R 755 /var/www/html/
 rm -rf /var/www/html/*
 
 sudo -u www-data sh -c "wp core download --path=/var/www/html/wordpress"
 
-echo "cd /var/www/html/wordpress"
-
 cd /var/www/html/wordpress
 
-# WordPress 설정
 sudo -u www-data sh -c "wp config create --dbhost=$DB_HOST \
                 --dbname=$DB_NAME \
                 --dbuser=$DB_USER \
                 --dbpass=$DB_PASSWORD"
 
-# WordPress 설치
 sudo -u www-data sh -c "wp core install --title=$WP_TITLE \
                 --admin_user=$WP_ADMIN_USER \
                 --admin_password=$WP_ADMIN_PASSWORD \
                 --admin_email=$WP_ADMIN_MAIL \
                 --url=$WP_URL"
 
-# 사용자 생성
 sudo -u www-data sh -c "wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD"
 
-# php-fpm 백그라운드로 실행
 php-fpm7.4 -F
